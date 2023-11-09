814f4d2b05c840c1db68e8c70558d859c024c177
Aron Jancso <aron.jancso@flowject.hu>
2023-11-09 19:16:57 +0100

    Remove boilerplate code

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,7 +1,8 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-
 import { AppRoutingModule } from './app-routing.module';
+
+// Components
 import { AppComponent } from './app.component';
 
 @NgModule({
